43280721559702970d50ede412e097be65ab23b4
ZaferSarioglu <sariogluuzafer@gmail.com>
2025-02-26 18:29:44 +0100

    Movies added.

--- src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
@@ -1,6 +1,7 @@
 package at.ac.fhcampuswien.fhmdb.models;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Movie {
@@ -9,22 +10,27 @@ public class Movie {
     // TODO add more properties here
     private List<Genres> genres;
 
-    public Movie(String title, String description) {
+    public Movie(String title, String description, List<Genres> genres) {
         this.title = title;
         this.description = description;
+        this.genres = new ArrayList<>();
     }
 
     public String getTitle() {
         return title;
     }
-
     public String getDescription() {
         return description;
+    }
+    public List<Genres> getGenres() {
+        return genres;
     }
 
     public static List<Movie> initializeMovies(){
         List<Movie> movies = new ArrayList<>();
         // TODO add some dummy data here
+        movies.add(new Movie("The Wolf of Wall Street", "asdadsasd",  Arrays.asList(Genres.DRAMA)));
+        movies.add(new Movie("Godfather","asdfadsfasf",  Arrays.asList(Genres.DRAMA)));
 
         return movies;
     }
