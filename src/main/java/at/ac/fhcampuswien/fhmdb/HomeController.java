0cb984183dbb5b3d8c4ec4424dc3c0d8e2f9869e
KaMa <maid.kanuric>
2025-03-02 12:27:30 +0100

    Refactor and add sorting

--- src/main/java/at/ac/fhcampuswien/fhmdb/HomeController.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/HomeController.java
@@ -8,14 +8,13 @@ import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXListView;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ResourceBundle;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class HomeController implements Initializable {
@@ -47,36 +46,50 @@ public class HomeController implements Initializable {
         movieListView.setCellFactory(movieListView -> new MovieCell()); // use custom cell factory to display data
 
         // TODO add genre filter items with genreComboBox.getItems().addAll(...)
-
-        genreComboBox.getItems().add("No filter");
-        for (Genres genre: Genres.values()) {
-            genreComboBox.getItems().add(genre);
-        }
         genreComboBox.setPromptText("Filter by Genre");
+        genreComboBox.getItems().add("No filter"); // no filter option
+        genreComboBox.getItems().addAll(Genres.values()); // fill with other genres
+
         //Event handlers
         // TODO add event handlers to buttons and call the regarding methods
+        // either set event handlers in the fxml file (onAction) or add them here
+
+        // Filter Button Pressed
         searchBtn.setOnAction(actionEvent -> {
             String searchQuery = searchField.getText().toLowerCase();
             String selectedGenre = genreComboBox.getValue() != null ? genreComboBox.getValue().toString() : null;
 
             filterMovies(searchQuery, selectedGenre);
-
         });
-        // either set event handlers in the fxml file (onAction) or add them here
 
-        // Sort button example:
+        // Sort Button Pressed
         sortBtn.setOnAction(actionEvent -> {
-            if(sortBtn.getText().equals("Sort (asc)")) {
+            boolean ascendingOrder;
+            if (sortBtn.getText().equals("Sort (asc)")) {
                 // TODO sort observableMovies ascending
                 sortBtn.setText("Sort (desc)");
+                ascendingOrder = false;
             } else {
                 // TODO sort observableMovies descending
                 sortBtn.setText("Sort (asc)");
+                ascendingOrder = true;
             }
+
+            sortMovies(ascendingOrder);
         });
 
 
     }
+
+    private void sortMovies(boolean ascendingOrder) {
+        Comparator<Movie> movieComparator = Comparator.comparing(Movie::getTitle);
+        if (ascendingOrder) {
+            observableMovies.sort(movieComparator);
+        } else {
+            observableMovies.sort(movieComparator.reversed());
+        }
+    }
+
     private void filterMovies(String searchQuery, String selectedGenre) {
 
         List<Movie> filteredList = new ArrayList<>(allMovies); //copy the original list to keep original list safe
