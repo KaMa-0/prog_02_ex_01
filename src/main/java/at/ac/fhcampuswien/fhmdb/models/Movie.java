132fbab09f484554ca2e226f42700592295ab096
sannas1 <slauted@gmail.com>
2025-03-02 14:31:53 +0100

    removed unnecessary attributes for movies

--- src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
@@ -6,29 +6,14 @@ import java.util.List;
 
 public class Movie {
 
-    private final int lengthInMinutes;
-    private final double rating;
     private String title;
     private String description;
-    private int releaseYear;
     private List<Genres> genres = new ArrayList<>();
 
-    public Movie(String title, String description, List<Genres> genres, int lengthInMinutes, double rating) {
+    public Movie(String title, String description, List<Genres> genres) {
         this.title = title;
         this.description = description;
-        this.lengthInMinutes = lengthInMinutes;
-        this.rating = rating;
         this.genres.addAll(genres);
-        this.releaseYear = 0;
-    }
-
-    public Movie(String title, String description, List<Genres> genres, int releaseYear, int lengthInMinutes, double rating) {
-        this.title = title;
-        this.description = description;
-        this.genres = genres;
-        this.releaseYear = releaseYear;
-        this.lengthInMinutes = lengthInMinutes;
-        this.rating = rating;
     }
 
     public String getTitle() {
@@ -49,26 +34,17 @@ public class Movie {
         movies.add(new Movie(
                 "Finding Nemo",
                 "After his son is captured in the Great Barrier Reef and taken to Sydney, a timid clownfish sets out on a journey to bring him home.",
-                Arrays.asList(Genres.ANIMATION, Genres.FAMILY, Genres.ADVENTURE, Genres.COMEDY),
-                2003,
-                100,
-                8.2));
+                Arrays.asList(Genres.ANIMATION, Genres.FAMILY, Genres.ADVENTURE, Genres.COMEDY)));
 
         movies.add(new Movie(
                 "Forrest Gump",
                 "The presidencies of Kennedy and Johnson, the Vietnam War, the Watergate scandal and other historical events unfold through the perspective of an Alabama man with an IQ of 75.",
-                Arrays.asList(Genres.DRAMA, Genres.ROMANCE, Genres.COMEDY),
-                1994,
-                142,
-                8.8));
+                Arrays.asList(Genres.DRAMA, Genres.ROMANCE, Genres.COMEDY)));
 
         movies.add(new Movie(
                 "Inception",
                 "A thief who enters the dreams of others to steal secrets from their subconscious is given a task to plant an idea into the mind of a CEO.",
-                Arrays.asList(Genres.ACTION, Genres.SCIENCE_FICTION, Genres.THRILLER),
-                2010,
-                148,
-                8.8));
+                Arrays.asList(Genres.ACTION, Genres.SCIENCE_FICTION, Genres.THRILLER)));
 
         return movies;
     }
