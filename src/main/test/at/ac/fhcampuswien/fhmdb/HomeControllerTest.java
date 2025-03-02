e69cf9d8835ded56a701d79184ba566d3e615781
KaMa <maid.kanuric>
2025-03-02 14:04:56 +0100

    Add basic testing comment tempalte

--- src/main/test/at/ac/fhcampuswien/fhmdb/HomeControllerTest.java
+++ src/main/test/at/ac/fhcampuswien/fhmdb/HomeControllerTest.java
@@ -1,7 +1,23 @@
 package at.ac.fhcampuswien.fhmdb;
 
+import org.junit.jupiter.api.BeforeAll;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HomeControllerTest {
+    // initialize homeController for testing
+    private static HomeController homeController;
+    @BeforeAll
+    static void init() {
+        homeController = new HomeController();
+    }
 
+    // Test strucuture
+    //@Test
+    //void descriptive_name_of_what_is_being_tested()
+    // given
+    // when
+        // should be 1-line -> otherwise split up
+    // then
+        // assertEquals
 }
